9baa54e387264748f189b1439a64a4749c9925d3
Mithila Dilshan <s92070213@ousl.lk>
2023-12-16 13:45:37 +0530

    Final commit

---
+++ queri.js
@@ -0,0 +1,8 @@
+function cls() {
+    document.getElementById("display").innerHTML;
+    document.getElementById("name").innerHTML;
+    document.getElementById("email").innerHTML;
+    document.getElementById("subject").innerHTML;
+    document.getElementById("contact").innerHTML;
+    document.getElementById("issue").innerHTML;
+}
